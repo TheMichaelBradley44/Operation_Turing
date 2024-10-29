ead6fa2c8ea8ba4d15f269bf25f749ab3313dcc0
TheMichaelBradley44 <michaelb44@live.co.uk>
2024-10-29 14:55:18 +0000

    Level Grid optimized

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -8,10 +8,17 @@ public class Unit : MonoBehaviour
     [SerializeField] private Animator unitAnimator;
 
     private Vector3 targetPosition;
+    private GridPosition gridPosition;
 
     private void Awake()
     {
         targetPosition = transform.position;
+    }
+
+    private void Start()
+    {
+        gridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        LevelGrid.Instance.AddUnitAtGridPosition(gridPosition, this);
     }
 
     private void Update()
@@ -34,7 +41,14 @@ public class Unit : MonoBehaviour
         {
             unitAnimator.SetBool("IsWalking", false);
         }
-        
+
+        GridPosition newGridPosition = LevelGrid.Instance.GetGridPosition(transform.position);
+        if (newGridPosition != gridPosition) 
+        {
+            LevelGrid.Instance.UnitMovedGridPosition(this, gridPosition, newGridPosition);
+            gridPosition = newGridPosition;
+        }
+
     }
 
     public void Move(Vector3 targetPosition)
