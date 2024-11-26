be2f63b0cc738bfc0c7f9993a26480e0b1c07f2d
TheMichaelBradley44 <michaelb44@live.co.uk>
2024-11-26 21:51:37 +0000

    UI fixed, Turn and Action point system implemented

---
+++ Assets/Scripts/Grid/TurnSystem.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnSystem : MonoBehaviour
+{
+
+    public static TurnSystem Instance { get; private set; }
+
+    private int turnNumber = 1;
+
+    public event EventHandler OnTurnChanged;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Debug.LogError("There's more than one TurnSystem! " + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+    }
+
+
+
+    public void NextTurn()
+    {
+        turnNumber++;
+
+        OnTurnChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    public int GetTurnNumber()
+    {
+        return turnNumber;
+    }
+
+}
