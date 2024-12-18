28ce3e44957bc1feef011409f13ea66b1e344114
TheMichaelBradley44 <michaelb44@live.co.uk>
2024-12-18 17:19:48 +0000

    Grenade Implemented

--- Assets/Scripts/UnitRagdoll.cs
+++ Assets/Scripts/UnitRagdoll.cs
@@ -4,13 +4,15 @@ using UnityEngine;
 
 public class UnitRagdoll : MonoBehaviour
 {
+
     [SerializeField] private Transform ragdollRootBone;
 
     public void Setup(Transform originalRootBone)
     {
         MatchAllChildTransforms(originalRootBone, ragdollRootBone);
 
-        ApplyExplosionToRagdoll(ragdollRootBone, 10f, transform.position, 10f);
+        Vector3 randomDir = new Vector3(Random.Range(-1f, +1f), 0, Random.Range(-1f, +1f));
+        ApplyExplosionToRagdoll(ragdollRootBone, 300f, transform.position + randomDir, 10f);
     }
 
     private void MatchAllChildTransforms(Transform root, Transform clone)
@@ -30,15 +32,16 @@ public class UnitRagdoll : MonoBehaviour
 
     private void ApplyExplosionToRagdoll(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRange)
     {
-        foreach(Transform child in root)
+        foreach (Transform child in root)
         {
             if (child.TryGetComponent<Rigidbody>(out Rigidbody childRigidbody))
-                {
+            {
                 childRigidbody.AddExplosionForce(explosionForce, explosionPosition, explosionRange);
-                }
+            }
 
             ApplyExplosionToRagdoll(child, explosionForce, explosionPosition, explosionRange);
         }
     }
 
-}   
+}
+
